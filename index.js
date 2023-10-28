10af5c7268b6a11281a6333b5406cc8182097af9
hmsmiraz <hmsmiraz64729@gmail.com>
2023-10-29 02:18:09 +0600

    added bookings

--- index.js
+++ index.js
@@ -47,8 +47,11 @@ async function run() {
 
 
     //bookings
-    app.post('/bookings', (req, res) =>{
+    app.post('/bookings', async (req, res) =>{
       const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
     });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
