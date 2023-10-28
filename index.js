1465acb5bba3c92579f3803759feee29d7da0782
hmsmiraz <hmsmiraz64729@gmail.com>
2023-10-28 13:17:05 +0600

    add booking collection

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+// const ObjectId = require("mongodb").ObjectId;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.meftkqt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,11 +27,28 @@ async function run() {
     await client.connect();
 
     const serviceCOllection = client.db('carDoctor').collection('services');
+    const bookingCollection = client.db('carDoctor').collection('bookings');
 
     app.get('/services', async(req, res)=>{
         const cursor = serviceCOllection.find();
         const result = await cursor.toArray();
         res.send(result);
+    });
+
+    app.get('/services/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query= { _id: new ObjectId(id)}
+      const options = {
+        projection: {title:1, price: 1, service_id: 1},
+      };
+      const result = await serviceCOllection.findOne(query, options);
+      res.send(result);
+    })
+
+
+    //bookings
+    app.post('/bookings', (req, res) =>{
+      const booking = req.body;
     });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
